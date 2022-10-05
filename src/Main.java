3aea482c9b4e3f59d8c50b8f96ccd2c347951b27
Victor Kwesiga <victorkwesiga141@gmail.com>
2022-10-05 07:43:11 +0300

    added another statement

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int z, x;
+        int z, x ,y;
         Scanner bob = new Scanner(System.in);
         System.out.println("Enter val one");
         z = bob.nextInt();
         System.out.println("Enter val two");
         x = bob.nextInt();
+        System.out.println("Enter val three");
+        y = bob.nextInt();
         if (z <= x) {
+            System.out.println("lets learn code");
+        }else if(z<=y){
             System.out.println("lets develop code");
-        }else {
-            System.out.println("lets share code");
         }
+        else { System.out.println("lets share code");}
     }
 }
